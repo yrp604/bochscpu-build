cc7b7f99dda67a46a62325b27d737d7c1db4223f
yrp <yrp604@protonmail.com>
2026-06-27 16:31:29 -0700

    update branch callbacks

--- bochscpu/instrument.h
+++ bochscpu/instrument.h
@@ -40,9 +40,8 @@ void bx_instr_mwait(unsigned cpu, bx_phy_address addr, unsigned len, Bit32u flag
 void bx_instr_debug_promt();
 void bx_instr_debug_cmd(const char *cmd);
 
-void bx_instr_cnear_branch_taken(unsigned cpu, bx_address branch_eip, bx_address new_eip);
-void bx_instr_cnear_branch_not_taken(unsigned cpu, bx_address branch_eip, bx_address new_eip);
-void bx_instr_ucnear_branch(unsigned cpu, unsigned what, bx_address branch_eip, bx_address new_eip);
+void bx_instr_branch_taken(unsigned cpu, unsigned what, bx_address branch_eip, bx_address new_eip);
+void bx_instr_branch_not_taken(unsigned cpu, unsigned what, bx_address branch_eip, bx_address new_eip);
 void bx_instr_far_branch(unsigned cpu, unsigned what, Bit16u prev_cs, bx_address prev_eip, Bit16u new_cs, bx_address new_eip);
 
 void bx_instr_opcode(unsigned cpu, bxInstruction_c *i, const Bit8u *opcode, unsigned len, bool is32, bool is64);
@@ -93,9 +92,8 @@ void bx_instr_vmexit(unsigned cpu, Bit32u reason, Bit64u qualification);
 #define BX_INSTR_DEBUG_CMD(cmd)          bx_instr_debug_cmd(cmd)
 
 /* branch resolution */
-#define BX_INSTR_CNEAR_BRANCH_TAKEN(cpu_id, branch_eip, new_eip) bx_instr_cnear_branch_taken(cpu_id, branch_eip, new_eip)
-#define BX_INSTR_CNEAR_BRANCH_NOT_TAKEN(cpu_id, branch_eip, new_eip) bx_instr_cnear_branch_not_taken(cpu_id, branch_eip, new_eip)
-#define BX_INSTR_UCNEAR_BRANCH(cpu_id, what, branch_eip, new_eip) bx_instr_ucnear_branch(cpu_id, what, branch_eip, new_eip)
+#define BX_INSTR_NEAR_BRANCH_TAKEN(cpu_id, what, branch_eip, new_eip) bx_instr_cnear_branch_taken(cpu_id, branch_eip, new_eip)
+#define BX_INSTR_NEAR_BRANCH_NOT_TAKEN(cpu_id, what, branch_eip, new_eip) bx_instr_cnear_branch_not_taken(cpu_id, branch_eip, new_eip)
 #define BX_INSTR_FAR_BRANCH(cpu_id, what, prev_cs, prev_eip, new_cs, new_eip) \
                        bx_instr_far_branch(cpu_id, what, prev_cs, prev_eip, new_cs, new_eip)
 
